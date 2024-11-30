5de9426096429a503e94cda896e4064f7c360561
Constantin Vaillant Tenzer <constantin.tenzer@ens.psl.eu>
2024-11-30 13:17:50 +0100

    Inverse kinematics and robots_benchmarks

---
+++ robot/show_in_meshcat_humanoids.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Author; Constantin Vaillant-Tenzer
+# Date: 2021-07-01
+# This script is used to show the URDF of the humanoids in MeshCat.
+
+
+robots_to_test=(
+    "atlas_drc_description" "atlas_v4_description" "draco3_description"
+    "ergocub_description" "g1_description" "g1_mj_description" "h1_description" "h1_mj_description"
+    "icub_description" "jaxon_description" "jvrc_description" "jvrc_mj_description" "op3_mj_description"
+    "r2_description" "romeo_description" "sigmaban_description" "talos_description" "talos_mj_description"
+    "valkyrie_description"
+)
+
+for robot in "${robots_to_test[@]}"; do
+    echo "Showing $robot in MeshCat"
+    python show_in_robomeshcat.py "$robot"
+done
